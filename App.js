58c4060eb34c412c516cf064a1fc50dc9dd516ef
Jamie Grantham-Burton <jamie.granthamburton@zebra.com>
2019-10-30 15:35:28 +0000

    Render list items based on input value submits with scroll view

--- App.js
+++ App.js
@@ -1,24 +1,63 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
 
 export default function App() {
-  
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    // console.log(enteredGoal);
+    setCourseGoals([...courseGoals, enteredGoal]);
+  }
+
   return (
-    <View style={{padding:30}}>
-      <View style={{flexDirection:'row', justifyContent:'space-between', alignItems: 'center'}}>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
         <TextInput 
-        placeholder="Course Goal..." 
-        style={{width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}}
+          placeholder="Course Goal..." 
+          style={styles.input} 
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
         />
-        <Button title="Add"/>
+        <Button title="Add" onPress={addGoalHandler}/>
       </View>
-      <View>
-     
-      </View>
+      <ScrollView>
+        {courseGoals.map((goal) => 
+          <View 
+            style={styles.listItem} 
+            key={goal}>
+              <Text>{goal}</Text>
+          </View>)}
+      </ScrollView>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
- 
+  screen: {
+    padding:50
+  },
+  inputContainer: {
+    flexDirection:'row', 
+    justifyContent:'space-between', 
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%', 
+    borderColor: 'black', 
+    borderWidth: 1, 
+    padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical:10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
 });
