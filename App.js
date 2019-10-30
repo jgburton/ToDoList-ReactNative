4d5d8a6f154ee0a980dff51cd0bc0073dc9e6416
Jamie Grantham-Burton <jamie.granthamburton@zebra.com>
2019-10-30 13:49:06 +0000

    basic concepts of react native: TextInput, Button with flex inline styling

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+export default function App() {
+  
+  return (
+    <View style={{padding:30}}>
+      <View style={{flexDirection:'row', justifyContent:'space-between', alignItems: 'center'}}>
+        <TextInput 
+        placeholder="Course Goal..." 
+        style={{width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}}
+        />
+        <Button title="Add"/>
+      </View>
+      <View>
+     
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
