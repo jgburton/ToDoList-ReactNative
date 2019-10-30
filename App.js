0799591637dcb43af150f94db4a83dada66ef658
Jamie Grantham-Burton <jamie.granthamburton@zebra.com>
2019-10-30 17:25:54 +0000

    split out into custom components

--- App.js
+++ App.js
@@ -1,65 +1,31 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState('');
   const [courseGoals, setCourseGoals] = useState([]);
-
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
-  };
-
-  const addGoalHandler = () => {
+ 
+  const addGoalHandler = goalTitle => {
     setCourseGoals(currentGoals => [
       ...courseGoals, 
-       {key: Math.random().toString(), value: enteredGoal }
+       {key: Math.random().toString(), value: goalTitle}
     ]);
   }
 
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput 
-          placeholder="Course Goal..." 
-          style={styles.input} 
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
-        />
-        <Button title="Add" onPress={addGoalHandler}/>
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
       <FlatList
         data={courseGoals} 
-        renderItem={itemData => (
-          <View style={styles.listItem}>
-            <Text>{itemData.item.value}</Text>
-        </View>
-      )} 
+        renderItem={itemData => ( <GoalItem title={itemData.item.value}/>)} 
     />
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
   screen: {
     padding:50
-  },
-  inputContainer: {
-    flexDirection:'row', 
-    justifyContent:'space-between', 
-    alignItems: 'center'
-  },
-  input: {
-    width: '80%', 
-    borderColor: 'black', 
-    borderWidth: 1, 
-    padding: 10
-  },
-  listItem: {
-    padding: 10,
-    marginVertical:10,
-    backgroundColor: '#ccc',
-    borderColor: 'black',
-    borderWidth: 1
-  }
+  }, 
 });
